7ea3ff6f824c1d2d158778d23b23a4c804f30fe8
rushitjasani <rushitjasani@gmail.com>
2018-09-18 10:51:44 +0530

    remove data on server when client requested.

--- client/client.cpp
+++ client/client.cpp
@@ -15,71 +15,139 @@
 #include "cl_global.h"
 #endif
 
-/*
- * Process command line arguments and stores
- * all ip/port in apropriate fields in global
- * variables.
- */
-void process_args(char *argv[])
+// void add_to_torrentlist(string data)
+// {
+//     // lock_guard<mutex> locker(my_mutex);
+//     fstream seeder_file;
+//     seeder_file.open(torrnet_record, fstream::app);
+//     seeder_file << data << endl;
+//     seeder_file.close();
+// }
+
+void notify_server(string filename, int sock)
 {
-    char *token = strtok(argv[1], ":");
-    cl_ip = token;
-    token = strtok(NULL, ":");
-    cl_port = stoi(token);
-
-    token = strtok(argv[2], ":");
-    tr1_ip = token;
-    token = strtok(NULL, ":");
-    tr1_port = stoi(token);
-
-    token = strtok(argv[3], ":");
-    tr2_ip = token;
-    token = strtok(NULL, ":");
-    tr2_port = stoi(token);
-
-    log_file = argv[4];
+    string msg, sh, f_path, s;
+    ifstream i_file;
+    i_file.open(filename);
+    for (int i = 0; i < 5; i++)
+    {
+        if (i == 0 || i == 1 || i == 3)
+        {
+            getline(i_file, s);
+            continue;
+        }
+        else if (i == 2)
+        {
+            getline(i_file, f_path);
+        }
+        else if (i == 4)
+        {
+            getline(i_file, sh);
+        }
+    }
+    i_file.close();
+    // cout << "File Path = " << f_path << endl;
+    sh = get_SHA1((char *)sh.c_str(), sh.size());
+    msg = "0|" + sh + "|" + cl_ip + ":" + to_string(cl_port) + "|" + f_path;
+    send(sock, msg.c_str(), msg.size(), 0);
+    cout << msg << endl;
+    cout << "MSG SENT" << endl;
 }
+
+void listen_for_client(){
+    //in this thread client will accept request and do all jobs.
+    return;
+}
+
+void getData(string file_path){
+
+}
+
+void remove_from_server(string torrent_file, int sock){
+    string msg, sh, f_path, s;
+    ifstream i_file;
+    i_file.open(torrent_file);
+    for (int i = 0; i < 5; i++)
+    {
+        if (i == 0 || i == 1 || i == 3)
+        {
+            getline(i_file, s);
+            continue;
+        }
+        else if (i == 2)
+        {
+            getline(i_file, f_path);
+        }
+        else if (i == 4)
+        {
+            getline(i_file, sh);
+        }
+    }
+    i_file.close();
+    // cout << "File Path = " << f_path << endl;
+    sh = get_SHA1((char *)sh.c_str(), sh.size());
+    msg = "1|" + sh + "|" + cl_ip + ":" + to_string(cl_port) + "|" + f_path;
+    send(sock, msg.c_str(), msg.size(), 0);
+    cout << msg << endl;
+    cout << "MSG SENT" << endl;
+}
+string create_absolute_path(string r_path)
+{
+    string abs_path = "";
+    if (r_path[0] == '.')
+    {
+        abs_path = string(cur_dir) + r_path.substr(1, r_path.length());
+    }
+    else if (r_path[0] == '/' || r_path[0] == '~')
+        ;
+    else
+        abs_path = string(cur_dir) + '/' + r_path;
+    // cout << abs_path << " ABS PATH" << endl;
+    return abs_path;
+}
+
 /*
  * Creates Socket by using provided ip and ports.
  */
 int soc_creation()
 {
     struct sockaddr_in tr1_addr;
-    struct sockaddr_in tr2_addr;
+    // struct sockaddr_in tr2_addr;
     int sock = 0;
 
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) <= 0)
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         cout << "Socket creation error" << endl;
         exit(EXIT_FAILURE);
     }
     memset(&tr1_addr, '0', sizeof(tr1_addr));
-    memset(&tr2_addr, '0', sizeof(tr2_addr));
+    // memset(&tr2_addr, '0', sizeof(tr2_addr));
 
     tr1_addr.sin_family = AF_INET;
-    tr2_addr.sin_family = AF_INET;
+    // tr2_addr.sin_family = AF_INET;
     tr1_addr.sin_port = htons(tr1_port);
-    tr2_addr.sin_port = htons(tr2_port);
+    // tr2_addr.sin_port = htons(tr2_port);
 
     if (inet_pton(AF_INET, tr1_ip.c_str(), &tr1_addr.sin_addr) <= 0)
     {
         cout << "Invalid Tracker 1 address/ Address not supported" << endl;
         exit(EXIT_FAILURE);
     }
-    if (inet_pton(AF_INET, tr2_ip.c_str(), &tr2_addr.sin_addr) <= 0)
-    {
-        cout << "Invalid Tracker 1 address/ Address not supported" << endl;
-        exit(EXIT_FAILURE);
-    }
-
+    // if (inet_pton(AF_INET, tr2_ip.c_str(), &tr2_addr.sin_addr) <= 0)
+    // {
+    //     cout << "Invalid Tracker 1 address/ Address not supported" << endl;
+    //     exit(EXIT_FAILURE);
+    // }
+    cout << "CONNECTING..." << endl;
     if (connect(sock, (struct sockaddr *)&tr1_addr, sizeof(tr1_addr)) < 0)
     {
         cout << " Tracker 1 seems Busy.. Trying to connect with Tracker 2.. " << endl;
-        if (connect(sock, (struct sockaddr *)&tr2_addr, sizeof(tr2_addr)) < 0)
-        {
-            cout << "Connection Failed :( \n Try again later" << endl;
-        }
+        // if (connect(sock, (struct sockaddr *)&tr2_addr, sizeof(tr2_addr)) < 0)
+        // {
+        //     cout << "Connection Failed :( \n Try again later" << endl;
+        // }
     }
+    cout << "CONNECTED with sock =  " << sock << endl;
     return sock;
 }
 
@@ -106,44 +174,34 @@ vector<string> split_command(string command_string)
     return my_command;
 }
 
-string create_absolute_path(string r_path)
-{
-    string abs_path = "";
-    if(r_path[0] == '.' ) {
-        abs_path = string(cur_dir) + r_path.substr(1, r_path.length());
-    }
-    else if( r_path[0] == '/' || r_path[0] == '~' );
-    else abs_path = string(cur_dir) + '/' + r_path;
-    // cout << abs_path << " ABS PATH" << endl;
-    return abs_path;
+void update_wakeup(){
+    //At time of wakeup, notify server about all available files.
+    return;
 }
 
-void notify_server(string filename, int sock)
+/*
+ * Process command line arguments and stores
+ * all ip/port in apropriate fields in global
+ * variables.
+ */
+void process_args(char *argv[])
 {
-    string msg, sh, f_path, s;
-    ifstream i_file;
-    i_file.open(filename);
-    for (int i = 0; i < 5; i++)
-    {
-        if (i == 0 || i == 1 || i == 3)
-        {
-            getline(i_file, s);
-            continue;
-        }
-        else if (i == 2)
-        {
-            getline(i_file, f_path);
-        }
-        else if (i == 4)
-        {
-            getline(i_file, sh);
-        }
-    }
-    cout << "File Path = " << f_path << endl;
-    sh = get_SHA1((char *)sh.c_str(), sh.size());
-    msg = sh + "|" + cl_ip + ":" + to_string(cl_port) + "|" + f_path;
-    send(sock, msg.c_str(), msg.size(), 0);
-    cout << "MSG SENT" << endl;
+    char *token = strtok(argv[1], ":");
+    cl_ip = token;
+    token = strtok(NULL, ":");
+    cl_port = stoi(token);
+
+    token = strtok(argv[2], ":");
+    tr1_ip = token;
+    token = strtok(NULL, ":");
+    tr1_port = stoi(token);
+
+    token = strtok(argv[3], ":");
+    tr2_ip = token;
+    token = strtok(NULL, ":");
+    tr2_port = stoi(token);
+
+    log_file = argv[4];
 }
 
 int main(int argc, char *argv[])
@@ -156,24 +214,56 @@ int main(int argc, char *argv[])
     }
     else
     {
-        getcwd( cur_dir, sizeof(cur_dir) );
+        getcwd(cur_dir, sizeof(cur_dir));
         process_args(argv);
-        int sock = soc_creation();
-        string sh_string = "share ./tmp_data/mycheck.pdf ./tmp_data/mycheck.mtorrent";
-        vector<string> command = split_command(sh_string);
-        // for(auto i:command) cout << i << endl;
-        if (command[0] == "share")
+        // bool isSharing=false;
+        update_wakeup();
+        //read stored .mtorrent file and notify server about it.
+        while (1)
         {
-            mtorrent_generator(create_absolute_path(command[1]) , command[2]);
-            notify_server(command[2], sock);
+            // string sh_string = "share ./tmp_data/mycheck.pdf ./tmp_data/mycheck.mtorrent";
+            cout << "Taking string" << endl;
+            string sh_string;
+            cin >> sh_string;
+            vector<string> command = split_command(sh_string);
+            for(auto i:command) cout << i << endl;
+            fflush(stdin);
+            fflush(stdout);
+            sleep(1);
+            if (command[0] == "share ")
+            {
+                cout << "in share command" << endl;
+                int sock_1 = soc_creation();
+                mtorrent_generator(create_absolute_path(command[1]), command[2]);
+                cout << "torrent generated." << endl;
+                notify_server(command[2], sock_1);
+                // if(!isSharing){
+                //     thread serverThread(listen_for_client);
+                //     isSharing = true;
+                // } 
+                close(sock_1);
+                continue;
+            }
+            if (command[0] == "get")
+            {
+                string torrent_file = command[1];
+                getData(torrent_file);
+                continue;
+            }
+            if (command[0] == "remove")
+            {
+                int sock_1 = soc_creation();
+                remove_from_server(command[1],sock_1);
+                remove(command[1].c_str());
+                close(sock_1);
+                continue;
+            }
+            if(command[0] == "exit"){
+                exit(0);
+            }
+            cout << "NOTHING MATCHED" << endl;
+
         }
-        // char rushit[] = "Hello there, This is Jasani :) ";
-        // send(sock, rushit, strlen(rushit), 0);
-        // cout << "MSG SENT" << endl;
-        char buffer[1024] = {0};
-        read(sock, buffer, 1024);
-        cout << buffer << endl;
-        //check for existing .mtorrent files
     }
     return 0;
 }
--- server/tracker.cpp
+++ server/tracker.cpp
@@ -16,30 +16,104 @@
 #include "server_global.h"
 #endif
 
-/*
- * Process command line arguments and stores
- * all ip/port in apropriate fields in global
- * variables.
- */
-void process_args(char *argv[])
+void print_map()
 {
-    char *token = strtok(argv[1], ":");
-    tr1_ip = token;
-    token = strtok(NULL, ":");
-    tr1_port = stoi(token);
-
-    token = strtok(argv[2], ":");
-    tr2_ip = token;
-    token = strtok(NULL, ":");
-    tr2_port = stoi(token);
-
-    seeder_list = argv[3];
-    log_file = argv[4];
+    cout << "================================================" << endl;
+    for (auto i : seeder_map)
+    {
+        cout << i.first << endl;
+        for (auto j : i.second)
+            cout << j.first << endl
+                 << j.second << endl;
+        cout << endl;
+    }
+    cout << "================================================" << endl;
 }
 
-struct sockaddr_in tr1_addr;
-// struct sockaddr_in tr2_addr;
+// mutex my_mutex;
+void add_to_seederlist(string data)
+{
+    // lock_guard<mutex> locker(my_mutex);
+    fstream seeder_file;
+    seeder_file.open(seeder_list, fstream::app);
+    seeder_file << data << endl;
+    seeder_file.close();
+}
 
+void write_to_seederlist()
+{
+    ofstream seed_file;
+    seed_file.open(seeder_list);
+    string s;
+    cout << "FILE WRITING START" << endl;
+    for (auto i : seeder_map)
+    {
+        for (auto j : i.second)
+            seed_file << i.first << "|" << j.first << "|" << j.second << endl;
+    }
+    seed_file.close();
+    cout << "FILE WRITING END" << endl;
+    print_map();
+    return;
+}
+
+void serve(int cl_soc)
+{
+    char buffer[1024] = {0};
+    read(cl_soc, buffer, 1024);
+    cout << buffer << endl;
+    vector<string> client_req;
+    char *token = strtok(buffer, "|");
+    while (token)
+    {
+        client_req.push_back(token);
+        token = strtok(NULL, "|");
+    }
+    string key_hash = client_req[1];
+    string cl_socket = client_req[2];
+    string file_path = client_req[3];
+    string buf_buf = key_hash + "|" + cl_socket + "|" + file_path;
+    if (client_req[0] == "0")
+    {
+        //share
+        if (seeder_map.find(key_hash) == seeder_map.end())
+        {
+            seeder_map[key_hash][cl_socket] = file_path;
+            add_to_seederlist(buf_buf);
+        }
+        else
+        {
+            if (seeder_map[key_hash].find(cl_socket) == seeder_map[key_hash].end())
+            {
+                seeder_map[key_hash][cl_socket] = file_path;
+                add_to_seederlist(buf_buf);
+            }
+        }
+    }
+    else if (client_req[0] == "1")
+    {
+        //remove
+        if (seeder_map.find(key_hash) != seeder_map.end())
+        {
+            map<string, string> temp;
+            temp = seeder_map[key_hash];
+            if (temp.size() == 1 && temp.find(cl_socket) != temp.end())
+            {
+                seeder_map.erase(key_hash);
+            }
+            else if (temp.size() != 1)
+            {
+                seeder_map[key_hash].erase(cl_socket);
+                write_to_seederlist();
+            }
+        }
+    }
+    else if (client_req[0] == "2")
+    {
+        //get
+    }
+    print_map();
+}
 int soc_creation()
 {
     struct sockaddr_in tr1_addr;
@@ -79,85 +153,13 @@ int soc_creation()
     cout << "LISTENING" << endl;
     return sock;
 }
-
-// mutex my_mutex;
-void add_entry_to_seederlist(string data)
-{
-    // lock_guard<mutex> locker(my_mutex);
-    fstream seeder_file;
-    seeder_file.open(seeder_list, fstream::app);
-    seeder_file << data << endl;
-    seeder_file.close();
-}
-
-bool isAvailable(vector<pair<string, string>> lst, pair<string, string> p)
-{
-    for (auto i : lst)
-    {
-        if (i.first == p.first && i.second == p.second)
-            return true;
-    }
-    return false;
-}
-
-
-void print_map(){
-    cout << "================================================" << endl;
-    for (auto i : seeder_map)
-    {
-        cout << i.first << endl;
-        for (auto j : i.second)
-            cout << j.first << endl
-                 << j.second << endl;
-        cout << endl;
-    }
-    cout << "================================================" << endl;
-}
-
-void serve(int cl_soc)
-{
-    char buffer[1024] = {0};
-    read(cl_soc, buffer, 1024);
-    string buf_buf = buffer;
-    cout << buffer << endl;
-    char *token = strtok(buffer, "|");
-    string key_hash = token;
-    token = strtok(NULL, "|");
-    string cl_socket = token;
-    token = strtok(NULL, "|");
-    string file_path = token;
-    // buffer[0]='\0';
-    if (seeder_map.find(key_hash) == seeder_map.end())
-    {
-        // seeder_map[key_hash].push_back({cl_socket, file_path});
-        cout << "I AM IN NOT MATCH" << endl;
-        seeder_map[key_hash][cl_socket] = file_path;
-        add_entry_to_seederlist(buf_buf);
-    }
-    else
-    {
-        // pair<string, string> new_pair = {cl_socket, file_path};
-        // if (!isAvailable(seeder_map[key_hash], new_pair))
-        if (seeder_map[key_hash].find(cl_socket) == seeder_map[key_hash].end())
-        {
-            cout << "I AM IN MATCH" << endl;
-            // seeder_map[key_hash].push_back({cl_socket, file_path});
-            seeder_map[key_hash][cl_socket] = file_path;
-            add_entry_to_seederlist(buf_buf);
-        }
-    }
-    print_map();
-    // char rushit[] = "Hello there, This is Jasani :) ";
-    // send(cl_soc, rushit, strlen(rushit), 0);
-    // cout << "MSG SENT" << endl;
-}
-
+struct sockaddr_in tr1_addr;
+// struct sockaddr_in tr2_addr;
 void read_seederlist()
 {
     ifstream seed_file;
     seed_file.open(seeder_list);
     string s;
-    // cout << "FILE READING START" << endl;
     while (getline(seed_file, s))
     {
         char *token = strtok((char *)s.c_str(), "|");
@@ -167,12 +169,30 @@ void read_seederlist()
         token = strtok(NULL, "|");
         string file_path = token;
         seeder_map[key_hash][cl_socket] = file_path;
-        // cout << " READING " << endl;
     }
     seed_file.close();
-    // cout << "FILE READING END" << endl;
     print_map();
     return;
+}
+/*
+ * Process command line arguments and stores
+ * all ip/port in apropriate fields in global
+ * variables.
+ */
+void process_args(char *argv[])
+{
+    char *token = strtok(argv[1], ":");
+    tr1_ip = token;
+    token = strtok(NULL, ":");
+    tr1_port = stoi(token);
+
+    token = strtok(argv[2], ":");
+    tr2_ip = token;
+    token = strtok(NULL, ":");
+    tr2_port = stoi(token);
+
+    seeder_list = argv[3];
+    log_file = argv[4];
 }
 
 int main(int argc, char *argv[])
@@ -193,10 +213,10 @@ int main(int argc, char *argv[])
         int cl_soc;
         while (1)
         {
-            cout << " WAITING FOR CLIENT with sock = " << sock << endl;
+            cout << " WAITING FOR CLIENT" << endl;
             if ((cl_soc = accept(sock, (struct sockaddr *)&tr1_addr, (socklen_t *)&tr1_addr)) < 0)
             {
-                cout << "Client Socket: " <<  cl_soc << endl;
+                cout << "Client Socket: " << cl_soc << endl;
                 perror("accept");
                 // exit(EXIT_FAILURE);
                 continue;
